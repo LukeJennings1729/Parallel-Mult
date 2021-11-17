e9ed67436f61f6563fb03169611f5a2d3f0c3d3d
LukeJennings1729 <lg04jenn@siena.edu>
2021-11-17 13:44:59 -0500

    first test of gmp library

--- mpi-mult/mult.c
+++ mpi-mult/mult.c
@@ -5,16 +5,52 @@
   Created: Nov 16, 2021
   Last Edited: Nov 16, 2021
   
-  Luke, Joseph, Zack
+  Luke Jennings, Joseph Capper, Zack Gilman
  */
 
 #include <errno.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <gmp.h> //for makefile, use -lgmp
 
+
+//integers in GMP are declared as mpz_t sum;
+
+
+int main(int argc, char *argv[]){
+  //this program is to test how to use the gmp library, and be able to successfully multiply large numbers. Things such as parallelization will be done later
+
+  //here is where we initialize our mpz_t, which stores our really large ints
+
+  printf("please input 2 integers:\n");
+  char* input1 = (char *)malloc(1000 * sizeof(char));
+  char* input2 = (char *)malloc(1000 * sizeof(char));
+  scanf("%s",input1);
+  scanf("%s",input2);
+
+  //here is where we initialize our mps_t variables
+  mpz_t big_int1, big_int2;
+  //the parameters for this are the mpz_t, the string, and the base of the string
+  mpz_init_set_str(big_int1,input1,10);
+  mpz_init_set_str(big_int2,input2,10);
+
+  //printing out the numbers
+  gmp_printf("%Zd\n",big_int1);
+  gmp_printf("%Zd\n",big_int2);
+
+  mpz_t output;
+  mpz_init(output);
+
+  mpz_mul(output,big_int1,big_int2);
+  
+  gmp_printf("%Zd\n",output);
+  return 0;
+  
+}
 // Should we use arrays to store numbers? (one digit per index)
 
+/*
 int count_digits(char *long_int) {
 
   int digits;
@@ -41,3 +77,4 @@ int main(int argc, char *argv[]) {
   printf("There are %d digits in massive_int_2\n", count_digits(massive_int_2));
   return 0;
 }
+*/
