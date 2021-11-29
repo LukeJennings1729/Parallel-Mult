9a5e1258e8e1111abeddc8d5f0281ebf0df1a393
josephcapper <jt05capp@siena.edu>
2021-11-29 16:49:36 -0500

    Files read correctly, error known

--- mpi-mult/mult.c
+++ mpi-mult/mult.c
@@ -17,22 +17,14 @@
 #include <unistd.h>
 #include "timer.h"
 
+#define MAX_BITS 32768
+
 //integers in GMP are declared as mpz_t sum;
 
-
-void mpz_rand(mpz_t in){
-  //change n to change size of random
-  mp_bitcnt_t n = 1000;
-  mpz_t rop;
-  gmp_randstate_t state;
-  mpz_init (rop);
-  gmp_randinit_mt(state);
-  long int op = time(NULL);
-  gmp_randseed_ui(state, op);
-  mpz_urandomb(in, state, n);
-}
-
 int main(int argc, char *argv[]){
+  char *input1  = (char *)malloc(MAX_BITS * sizeof(char));
+  char *input2 = (char *)malloc(MAX_BITS * sizeof(char));
+  FILE *in_file1, *in_file2;
   //this program is to test how to use the gmp library, and be able to successfully multiply large numbers. Things such as parallelization will be done later
 
   //here is where we initialize our mpz_t, which stores our really large ints
@@ -45,15 +37,36 @@ int main(int argc, char *argv[]){
     scanf("%s",input2);
   */
 
-  
   //here is where we initialize our mps_t variables
   mpz_t big_int1, big_int2;
+
+  
+  in_file1 = fopen(argv[1],"r");
+  in_file2 = fopen(argv[2],"r");
+  //if in_file is still NULL
+  if(in_file1 == NULL){
+    fprintf(stderr,"[%s] could not open file 1 %s\n",argv[0], argv[1]);
+    exit(1);
+  }
+  if(in_file2 == NULL){
+    fprintf(stderr,"[%s] could not open file 2 %s\n",argv[0], argv[2]);
+    exit(1);
+  }
+  
+  fscanf(in_file1,"%s",input1);
+  fscanf(in_file2,"%s",input2);
+
+  mpz_set_str(big_int1,input1,16);
+  mpz_set_str(big_int2,input2,16);
+  
+  /*
   //the parameters for this are the mpz_t, the string, and the base of the string
   mpz_init(big_int1);
   mpz_rand(big_int1);
   sleep(1);
   mpz_init(big_int2);
   mpz_rand(big_int2);
+  */
   //printing out the numbers
   mpz_t output;
   gmp_printf("int 1:%Zd \nint 2:%Zd\n",big_int1,big_int2);
