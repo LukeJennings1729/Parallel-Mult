cb7db8314b9358b3a5d0760b24defa9450da0488
josephcapper <jt05capp@siena.edu>
2021-12-03 15:36:07 -0500

    serial version of thread-mult

--- mpi-mult/mult.c
+++ mpi-mult/mult.c
@@ -17,7 +17,7 @@
 #include <unistd.h>
 #include "timer.h"
 
-#define MAX_BITS 32768
+#define MAX_BITS 1073741824
 
 //integers in GMP are declared as mpz_t sum;
 
@@ -25,22 +25,18 @@ int main(int argc, char *argv[]){
   char *input1  = (char *)malloc(MAX_BITS * sizeof(char));
   char *input2 = (char *)malloc(MAX_BITS * sizeof(char));
   FILE *in_file1, *in_file2;
-  //this program is to test how to use the gmp library, and be able to successfully multiply large numbers. Things such as parallelization will be done later
+  struct timeval start, stop;
 
-  //here is where we initialize our mpz_t, which stores our really large ints
-  
-  /*
-    printf("please input 2 integers:\n");
-    char* input1 = (char *)malloc(10000 * sizeof(char));
-    char* input2 = (char *)malloc(10000 * sizeof(char));
-    scanf("%s",input1);
-    scanf("%s",input2);
-  */
+  //this program is to test how to use the gmp library, and be able to successfully multiply large numbers.
 
   //here is where we initialize our mps_t variables
   mpz_t big_int1, big_int2;
 
-  
+  if(argc < 3){
+    fprintf(stderr,"[%s] USAGE: FILE file1, File file2 \n",argv[0]);
+    exit(1);
+  }
+
   in_file1 = fopen(argv[1],"r");
   in_file2 = fopen(argv[2],"r");
   //if in_file is still NULL
@@ -52,59 +48,33 @@ int main(int argc, char *argv[]){
     fprintf(stderr,"[%s] could not open file 2 %s\n",argv[0], argv[2]);
     exit(1);
   }
-  
+
+  //scan numbers from files
   fscanf(in_file1,"%s",input1);
   fscanf(in_file2,"%s",input2);
 
+  gettimeofday(&start, NULL);
+ 
+  mpz_init(big_int1);
+  mpz_init(big_int2);
   mpz_set_str(big_int1,input1,16);
   mpz_set_str(big_int2,input2,16);
-  
-  /*
-  //the parameters for this are the mpz_t, the string, and the base of the string
-  mpz_init(big_int1);
-  mpz_rand(big_int1);
-  sleep(1);
-  mpz_init(big_int2);
-  mpz_rand(big_int2);
-  */
+ 
   //printing out the numbers
   mpz_t output;
-  gmp_printf("int 1:%Zd \nint 2:%Zd\n",big_int1,big_int2);
+  //gmp_printf("int 1:%Zd \nint 2:%Zd\n",big_int1,big_int2);
   mpz_init(output);
 
+  //Do the multiplication
   mpz_mul(output,big_int1,big_int2);
-  
-  gmp_printf("Output: %Zd\n",output);
+
+  gettimeofday(&stop, NULL);
+
+  //Print the output
+  gmp_printf("%Zd\n",output);
+
+  printf("Time Elapsed: %f\n", diffgettime(start, stop));
+
   return 0;
   
 }
-// Should we use arrays to store numbers? (one digit per index)
-
-/*
-int count_digits(char *long_int) {
-
-  int digits;
-  digits = 0;
-  while (long_int[digits] != '\0')
-    digits++;
-  return digits;
-}
-
-int main(int argc, char *argv[]) {
-
-  char *massive_int_1, *massive_int_2;
-  //char *product;
-  
-  if (argc != 3) {
-    fprintf(stderr, "Usage: ./%s massive_int_1 massive_int_2\n", argv[0]);
-    exit(1);
-  }
-
-  massive_int_1 = argv[1];
-  massive_int_2 = argv[2];
-  
-  printf("There are %d digits in massive_int_1\n", count_digits(massive_int_1));
-  printf("There are %d digits in massive_int_2\n", count_digits(massive_int_2));
-  return 0;
-}
-*/
